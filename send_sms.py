eb34eff29deb13671b389c5538c67ed85f3bf812
TL836 <31741598+DuanLiu836@users.noreply.github.com>
2018-01-28 03:42:51 -0600

    Add files via upload

---
+++ send_sms.py
@@ -0,0 +1,15 @@
+from twilio.rest import Client
+
+
+account_sid = "AC9b64644cb7c33ef1467ab5fba7aa385f"
+
+auth_token  = "b88f5ffb3a8fe815538506ebed83b3d6"
+
+client = Client(account_sid, auth_token)
+
+message = client.messages.create(
+    to = "+17853805890",
+    from_ = "+17165314545",
+    body = ("TestMessage!"))
+
+print(message.sid)
